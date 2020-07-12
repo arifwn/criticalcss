4807b92886e77553e9604c1ac884e9ff64ab8dad
Arif Widi Nugroho <arif@sainsmograf.com>
2020-07-13 00:47:20 +0700

    increased penthouse page load timeout to 30s

--- critical-generator.js
+++ critical-generator.js
@@ -4,46 +4,42 @@
 const critical = require('critical');
 
 const generateCritical = async (url) => {
-  try {
-    const startTime = Math.round(+new Date()/1000);
-    console.log('generating critical css for', url);
-    const {css, html, uncritical} = await critical.generate({
-      strict: false,
-      rebase: false,
-      minify: false,
-      base: 'dist',
-      src: url,
-      penthouse: {
-        timeout: 90000,
+  const startTime = Math.round(+new Date()/1000);
+  console.log('generating critical css for', url);
+  const {css, html, uncritical} = await critical.generate({
+    strict: false,
+    rebase: false,
+    minify: false,
+    base: 'dist',
+    src: url,
+    penthouse: {
+      timeout: 90000,
+      pageLoadSkipTimeout: 30000,
+    },
+    dimensions: [
+      {
+        height: 375,
+        width: 812
       },
-      dimensions: [
-        {
-          height: 375,
-          width: 812
-        },
-        {
-          height: 768,
-          width: 1024
-        },
-        {
-          height: 1280,
-          width: 800
-        },
-        {
-          height: 1980,
-          width: 1280
-        },
-      ]
-    });
+      {
+        height: 768,
+        width: 1024
+      },
+      {
+        height: 1280,
+        width: 800
+      },
+      {
+        height: 1980,
+        width: 1280
+      },
+    ]
+  });
 
-    const endTime = Math.round(+new Date()/1000);
-    const totalTime = endTime - startTime;
-    console.log(`done in ${totalTime}s`);
-    return css;
-  } catch (error) {
-    console.log('err:', error);
-    reject(error);
-  }
+  const endTime = Math.round(+new Date()/1000);
+  const totalTime = endTime - startTime;
+  console.log(`done in ${totalTime}s`);
+  return css;
 }
 
 // const generateCritical = (url) => {
